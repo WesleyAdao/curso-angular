d53d8cb00a9b9225d252dfef3c394636df8e90fe
Wesley Adao <wesleyadao88@hotmail.com>
2021-02-17 22:41:29 -0300

    feat: property binding no header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-    pudim = "pudim";
-    
+  mensagemTitulo = 'Seja bem vindo';
+  pudim = "pudim";
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() titulo!: string;
+
   constructor() { }
 
   ngOnInit(): void {
 
   }
 
-  title = 'Seja bem vindo';
+  
 
 
 }
